f7eebd1c701aef48522e9df29f839cfc44fb07dd
graveyard233 <382287750@qq.com>
2022-04-12 23:27:35 +0800

    修改了时间选择的样式，但仍未完成

--- app/src/main/java/com/example/hotel/UI/Room/Activity_book_the_room.java
+++ app/src/main/java/com/example/hotel/UI/Room/Activity_book_the_room.java
@@ -9,7 +9,11 @@ import android.widget.TextView;
 
 import com.example.hotel.R;
 import com.example.hotel.UI.Base.BaseActivity;
+import com.loper7.date_time_picker.DateTimeConfig;
 import com.loper7.date_time_picker.dialog.CardDatePickerDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity_book_the_room extends BaseActivity implements View.OnClickListener {
 
@@ -32,8 +36,17 @@ public class Activity_book_the_room extends BaseActivity implements View.OnClick
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.bookTheRoom_choiceStart:{
+                //设置时间显示
+                List<Integer> displayList = new ArrayList<>();
+                displayList.add(DateTimeConfig.YEAR);
+                displayList.add(DateTimeConfig.MONTH);
+                displayList.add(DateTimeConfig.DAY);
+                displayList.add(DateTimeConfig.HOUR);
+
                 new CardDatePickerDialog.Builder(this)
                         .setTitle("title")
+                        .setThemeColor(getResources().getColor(R.color.深竹月_浅色))
+                        .setDisplayType(displayList)
                         .setOnChoose("确定",aLong -> {
                             return null;
                         }).build().show();
