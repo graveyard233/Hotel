dab95891b6bb4f1a27bf44fcb1e2cde80f91b71a
graveyard233 <382287750@qq.com>
2022-04-15 13:57:45 +0800

    已经实现了样式，准备实现穿透点击

--- app/src/main/java/com/example/hotel/UI/Order/OrderFragment.java
+++ app/src/main/java/com/example/hotel/UI/Order/OrderFragment.java
@@ -1,5 +1,9 @@
 package com.example.hotel.UI.Order;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
 import com.example.hotel.R;
 import com.example.hotel.UI.Base.BaseFragment;
 import com.example.hotel.VedioPlayingIcon;
@@ -7,10 +11,22 @@ import com.example.hotel.VedioPlayingIcon;
 public class OrderFragment extends BaseFragment {
 
     private VedioPlayingIcon icon;
+    private VedioPlayingIcon icon2;
+
+    private TextView textView;
     @Override
     protected void initViews() {
         icon = find(R.id.my_icon);
+        icon2 = find(R.id.my_icon2);
         icon.start();
+        icon2.start();
+        textView = find(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("TAG", "onClick: 1");
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/hotel/VedioPlayingIcon.java
+++ app/src/main/java/com/example/hotel/VedioPlayingIcon.java
@@ -1,12 +1,16 @@
 package com.example.hotel;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -34,7 +38,7 @@ public class VedioPlayingIcon extends View {
     private float pointerPadding;
 
     //指针宽度
-    private float pointerWidth = 30;
+    private float pointerWidth;
 
     //指针颜色
     private int pointerColor = Color.WHITE;
@@ -46,10 +50,14 @@ public class VedioPlayingIcon extends View {
     private Thread myThread;
 
     //跳动速度
-    private int pointerSpeed = 35;
+    private int pointerSpeed = 30;
+
+    //指针同步率
+    private float sync = 0.575f;
 
     public VedioPlayingIcon(Context context) {
         super(context);
+
         init();
     }
 
@@ -57,7 +65,14 @@ public class VedioPlayingIcon extends View {
 
     public VedioPlayingIcon(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+//        pointerWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,getWidth(),
+//                context.getResources().getDisplayMetrics());
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.VedioPlayingIcon);
+        pointerWidth = array.getLayoutDimension(
+                R.styleable.VedioPlayingIcon_android_layout_width, 28 * 5) / 5;
+        array.recycle();
         init();
+
     }
 
     public VedioPlayingIcon(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -85,12 +100,12 @@ public class VedioPlayingIcon extends View {
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
         //获取逻辑原点，画布的左下角,要减去padding bottom的距离
-        basePointY = getHeight() - getPaddingBottom();
+        basePointY = getHeight() - pointerWidth / 2 ;
         Random random = new Random();
         if (pointers != null){
             pointers.clear();
         }
-        float j = 0.1f;
+        float j = 0.2f;
         for (int i = 0; i < pointerNum; i++) {
 
             Pointer pointer = new Pointer(
@@ -98,18 +113,21 @@ public class VedioPlayingIcon extends View {
                     )
             );
             pointers.add(pointer);
-            j += 0.4f;
+            j += 0.3f;
         }
 
         //指针之间的间隔
-        pointerPadding = (getWidth() - getPaddingLeft() - getPaddingRight() - pointerWidth * pointerNum)
-                / (pointerNum - 1);
+//        pointerPadding = (float) ((getWidth() - getPaddingLeft() - getPaddingRight() - pointerWidth * pointerNum)
+//                        / (pointerNum - 1));
+        //间隔为柱子宽度的一半
+        pointerPadding = pointerWidth / 2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        basePointX = 0f + getPaddingLeft();
+//        basePointX = (float) (0f + getPaddingLeft() * 7);
+        basePointX = (float) (0f + pointerPadding * 2);
         for (int i = 0; i < pointers.size(); i++) {
 //            canvas.drawRect(basePointX,
 //                    basePointY - pointers.get(i).getHeight(),
@@ -119,7 +137,7 @@ public class VedioPlayingIcon extends View {
             canvas.drawLine(basePointX,
                     basePointY,
                     basePointX,
-                    basePointY - pointers.get(i).getHeight(),
+                    basePointY - pointers.get(i).getHeight() ,
                     paint);
             //加上padding 往右移动
             basePointX += (pointerPadding + pointerWidth);
@@ -160,9 +178,9 @@ public class VedioPlayingIcon extends View {
                     for (int j = 0; j < pointers.size(); j++) {
                         //改变指针高度
                         //利用正弦规律获取0到1
-                        float rate = (float) Math.abs(Math.cos(i + j));
+                        float rate = (float) Math.abs(Math.sin(i + j * sync));
                         //有规律的改变高度
-                        pointers.get(j).setHeight((basePointY - getPaddingTop()) * rate);
+                        pointers.get(j).setHeight((basePointY - getPaddingTop()) * rate * 0.8f);
                     }
                     Thread.sleep(pointerSpeed);
                     if (isPlaying){
@@ -176,6 +194,7 @@ public class VedioPlayingIcon extends View {
             }
         }
     }
+
 
     public class Pointer {
         private float height;
@@ -192,4 +211,11 @@ public class VedioPlayingIcon extends View {
             this.height = height;
         }
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        return true;
+    }
+
+
 }
