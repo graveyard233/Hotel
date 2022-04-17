6fdda41572f6a5e9d8a725f96918619d678299e1
graveyard233 <382287750@qq.com>
2022-04-17 23:31:18 +0800

    正在完善订单展示界面

--- app/src/main/java/com/example/hotel/UI/Order/OrderFragment.java
+++ app/src/main/java/com/example/hotel/UI/Order/OrderFragment.java
@@ -15,40 +15,10 @@ import com.example.hotel.VedioPlayingIcon;
 
 public class OrderFragment extends BaseFragment {
 
-    VedioPlayingIcon icon;
 
-    private TextView textView;
-    private LinearLayout view;
-    private ConstraintLayout layout;
     @Override
     protected void initViews() {
-        icon = find(R.id.my_icon);
-//        icon2 = find(R.id.my_icon2);
-        icon.setPointerSpeed(40);
-        icon.setPointerColor(Color.RED);
-        view = find(R.id.mytest);
-//        view.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Log.i("TAG", "onClick: 2");
-//            }
-//        });
-        textView = find(R.id.textView);
-//        textView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Log.i("TAG", "onClick: 1");
-//            }
-//        });
 
-        layout = find(R.id.maxlayout);
-        layout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                Log.i("TAG", "onTouch: x = " + motionEvent.getX() + ",y = " + motionEvent.getY());
-                return false;
-            }
-        });
     }
 
     @Override
