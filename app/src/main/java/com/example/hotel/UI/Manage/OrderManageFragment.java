511f6a7bea8f89d669093886202b191fb5fcd553
graveyard233 <382287750@qq.com>
2022-04-19 23:19:27 +0800

    着手使用navigation实现导航

---
+++ app/src/main/java/com/example/hotel/UI/Manage/OrderManageFragment.java
@@ -0,0 +1,27 @@
+package com.example.hotel.UI.Manage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hotel.R;
+import com.example.hotel.UI.Base.BaseFragment;
+
+
+public class OrderManageFragment extends BaseFragment {
+
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_order_manage;
+    }
+}
---
+++ app/src/main/java/com/example/hotel/UI/Manage/RoomManageFragment.java
@@ -0,0 +1,26 @@
+package com.example.hotel.UI.Manage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hotel.R;
+import com.example.hotel.UI.Base.BaseFragment;
+
+
+public class RoomManageFragment extends BaseFragment {
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_room_manage;
+    }
+}
