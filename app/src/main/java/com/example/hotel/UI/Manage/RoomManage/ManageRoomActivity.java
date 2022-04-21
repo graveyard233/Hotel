6bb73528b5201ea99f55493eec05d3b747c922dd
graveyard233 <382287750@qq.com>
2022-04-21 23:36:01 +0800

    存档，那个日历需要初始化才能展示，否则闪退

--- app/src/main/java/com/example/hotel/UI/Manage/RoomManage/ManageRoomActivity.java
+++ app/src/main/java/com/example/hotel/UI/Manage/RoomManage/ManageRoomActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.hotel.Bean.Room;
 import com.example.hotel.R;
 import com.example.hotel.UI.Base.BaseActivity;
+import com.google.gson.Gson;
 
 public class ManageRoomActivity extends BaseActivity {
 
+    private Room room_this;
 
     @Override
     protected void initViews() {
+        String roomJson = getIntent().getStringExtra("roomJson");
+        Gson gson = new Gson();
+        room_this = gson.fromJson(roomJson,Room.class);
+
 
     }
 
