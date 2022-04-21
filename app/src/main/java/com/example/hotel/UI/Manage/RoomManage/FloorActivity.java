c097336152a842ec7ced127083cdbd2ed698c444
graveyard233 <382287750@qq.com>
2022-04-21 19:57:28 +0800

    存档 准备实现房间编辑界面

--- app/src/main/java/com/example/hotel/UI/Manage/RoomManage/FloorActivity.java
+++ app/src/main/java/com/example/hotel/UI/Manage/RoomManage/FloorActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.hotel.Bean.Room;
 import com.example.hotel.R;
 import com.example.hotel.UI.Base.BaseActivity;
 import com.example.hotel.UI.Manage.RoomManage.adapter.FloorRecyclerViewAdapter;
+import com.example.hotel.UI.Manage.UserManage.SingleUserInfoActivity;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +55,18 @@ public class FloorActivity extends BaseActivity {
                 adapter = new FloorRecyclerViewAdapter(rooms);
                 recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
                 recyclerView.setAdapter(adapter);
+
+                adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                        Log.i("TAG", "onItemClick: " + position);
+                        Intent to_manage_room = new Intent(getApplicationContext(), ManageRoomActivity.class);
+                        Gson gson = new Gson();
+                        String roomJson = gson.toJson(adapter.getItem(position));
+                        to_manage_room.putExtra("roomJson",roomJson);
+                        startActivity(to_manage_room);
+                    }
+                });
             }
         });
     }
